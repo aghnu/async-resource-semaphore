7753e716d27c33129d0b6d7f512ae160ba266bcd
Gengyuan Huang <46549455+aghnu@users.noreply.github.com>
2024-01-06 16:49:49 -0700

    Setup jest ts and vite config

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+// vite.config.ts
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+// https://vitejs.dev/guide/build.html#library-mode
+export default defineConfig({
+  build: {
+    emptyOutDir: true,
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "async-semaphore",
+      fileName: "async-semaphore",
+    },
+  },
+  plugins: [
+    dts({
+      exclude: ["./vite.config.ts", "./.eslintrc.cjs", "./jest.config.cjs"],
+    }),
+  ],
+});
